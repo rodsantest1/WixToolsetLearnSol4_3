97f0245f730dc1279707c5dfab36f3808df9ef5b
Rodney Santiago <GD000007711@ups.com>
2024-04-25 18:13:49 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static ServiceProvider _serviceProvider;
+
+        static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(AddConfiguration());
+        }
+
+        static IConfiguration AddConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+#if DEV
+            builder.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+#elif PROD
+            builder.AddJsonFile("appsettings.Production.json", optional: true, reloadOnChange: true);
+#endif
+
+            return builder.Build();
+        }
+
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+
+            var appsettings = _serviceProvider.GetService<IConfiguration>();
+
+            var configHello = $"Hello {appsettings?.GetValue<string>("hello")}!";
+
+            Console.WriteLine(configHello);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# WixToolsetLearnSol4
+
+4/13/24  
+Got it to work (it appears) [[Go]](https://github.com/rodsantest1/WixToolsetLearnSol4/commit/169e653b62af70e1e05918d1e4179caa0307f25e)  
+
+4/12/24  
+
+2:00:00 Summary
+
+1:35:00 Heat
+
+https://github.com/wixtoolset/issues/issues/6636
+
+1:17:00 Starting BeforeBuild target
+
+
+1:14:00 Recap  
+https://youtu.be/6Yf-eDsRrnM?si=PikZg_yMMS11aqdj&t=4448  
